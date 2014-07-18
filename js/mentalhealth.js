847dd219079f1bb05354d662a38fc7dbe93cfea3
noyschaal <noyschaal@gmail.com>
2014-07-18 18:21:27 -0400

    mental health form done

---
+++ js/mentalhealth.js
@@ -0,0 +1,25 @@
+var user={firstname:"",lastname:"", age:0, sleep:0, smoke:0, drink:0, exercise:0, stress:0, happy:0 };
+var hobbies=[];
+
+function submitForm(){
+	user.firstname=document.getElementById('firstname').value;
+	user.lastname=document.getElementById('lastname').value;
+	user.age=document.getElementById('age').value;
+	user.sleep=document.getElementById('sleep').value;
+	user.smoke=document.getElementById('smoke').value;
+	user.drink=document.getElementById('drink').value;
+	user.exercise=document.getElementById('exercise').value;
+	user.stress=document.getElementById('stress').value;
+	user.happy=document.getElementById('happy').value;
+	alert("test");
+	var i;
+	for(i=0; i<20; i++){
+		var num=i.toString();
+		alert(num);
+		var idString=num+"C";
+		if(document.getElementById(idString).checked){
+			hobbies.push(document.getElementById(idString).value);
+			alert(hobbies.toString());
+		}
+	}
+}
---
+++ js/physicalhealth.js
@@ -0,0 +1,32 @@
+var person = {
+	height:"",
+	weight:"",	
+};
+
+function saveInfo() {
+
+	if(document.getElementById('myForm').checkValidity()=== false)
+	{
+		return false;
+	}
+
+	// person.firstName = document.getElementById('first').value;
+	// person.lastName = document.getElementById('last').value;
+	// person.age = document.getElementById('age').value;
+	// person.tall = document.getElementById('height').value;
+	// person.weight = document.getElementById('weight').value;
+	// person.gender = document.getElementById('gender').value;
+	return true;
+
+}
+
+
+function calcBMI() {
+      person.height = document.getElementById('height').value;
+      person.weight = document.getElementById('weight').value;
+      var bmi = (person.weight * 703) / (person.height * person.height);
+
+      document.getElementById('bmi').innerText=" Your BMI is " + Math.round(bmi*10)/10;
+     return false;
+}
+      
